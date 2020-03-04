cb86b2b1161e821ee4488e37eb131fcb2b735b90
Decio Ferreira <decio.ferreira@decioferreira.com>
2020-03-04 11:35:09 +0000

    Fixes #103 Html.map + blur event on DOM removal makes impossible states possible

--- src/Elm/Kernel/VirtualDom.js
+++ src/Elm/Kernel/VirtualDom.js
@@ -589,6 +589,10 @@ function _VirtualDom_applyEvents(domNode, eventNode, events)
 			if (oldHandler.$ === newHandler.$)
 			{
 				oldCallback.__handler = newHandler;
+				oldCallback.__eventNode = {
+					__tagger: eventNode.__tagger,
+					__parent: eventNode.__parent
+				};
 				continue;
 			}
 			domNode.removeEventListener(key, oldCallback);
@@ -648,7 +652,7 @@ function _VirtualDom_makeCallback(eventNode, initialHandler)
 		var currentEventNode = (
 			stopPropagation && event.stopPropagation(),
 			(tag == 2 ? value.b : tag == 3 && value.__$preventDefault) && event.preventDefault(),
-			eventNode
+			callback.__eventNode
 		);
 		var tagger;
 		var i;
@@ -671,6 +675,7 @@ function _VirtualDom_makeCallback(eventNode, initialHandler)
 	}
 
 	callback.__handler = initialHandler;
+	callback.__eventNode = { __tagger: eventNode.__tagger, __parent: eventNode.__parent };
 
 	return callback;
 }
